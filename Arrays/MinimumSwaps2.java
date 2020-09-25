6a7aa08770ae4bbf0b495043d8dfa0df780767e5
Mac User <macuser@MacintoshUsers-MacBook-2.local>
2020-09-25 12:30:25 -0700

    Added the problem description

--- Arrays/MinimumSwaps2.java
+++ Arrays/MinimumSwaps2.java
@@ -9,6 +9,28 @@ import java.util.regex.*;
 public class MinimumSwaps2{
 
     // Complete the minimumSwaps function below.
+    /*
+    5
+    2 3 4 1 5
+
+    o/p: 3
+
+    You are given an unordered array consisting of consecutive integers  [1, 2, 3, ..., n] without any duplicates.
+    You are allowed to swap any two elements. You need to find the minimum number of swaps required to sort
+    the array in ascending order.
+
+    For example, given the array  we perform the following steps:
+
+i   arr                         swap (indices)
+0   [7, 1, 3, 2, 4, 5, 6]   swap (0,3)
+1   [2, 1, 3, 7, 4, 5, 6]   swap (0,1)
+2   [1, 2, 3, 7, 4, 5, 6]   swap (3,4)
+3   [1, 2, 3, 4, 7, 5, 6]   swap (4,5)
+4   [1, 2, 3, 4, 5, 7, 6]   swap (5,6)
+5   [1, 2, 3, 4, 5, 6, 7]
+It took 5 swaps to sort the array.
+
+     */
     static int minimumSwaps(int[] arr) {
         int temp;
         int swaps=0;
