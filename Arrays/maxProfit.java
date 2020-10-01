3d3d1a8937de40031ec7948666cbb5cfb1833a47
Mac User <macuser@MacintoshUsers-MacBook-2.local>
2020-09-30 18:41:00 -0700

    Adding max profit

---
+++ Arrays/maxProfit.java
@@ -0,0 +1,61 @@
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
+
+public class maxProfit {
+    /*
+    Say you have an array for which the ith element is the price of a given stock on day i.
+
+If you were only permitted to complete at most one transaction (i.e., buy one and sell one share of the stock),
+design an algorithm to find the maximum profit.
+
+Note that you cannot sell a stock before you buy one.
+
+Example 1:
+
+Input: [7,1,5,3,6,4]
+Output: 5
+Explanation: Buy on day 2 (price = 1) and sell on day 5 (price = 6), profit = 6-1 = 5.
+             Not 7-1 = 6, as selling price needs to be larger than buying price.
+Example 2:
+
+Input: [7,6,4,3,1]
+Output: 0
+Explanation: In this case, no transaction is done, i.e. max profit = 0.
+     */
+    public static int maxProfit(int[] nums) {
+        int max = 0;
+        int lst = Integer.MAX_VALUE;
+        int profit = 0;
+        for(int i=0;i<nums.length;i++){
+            lst = Math.min(lst,nums[i]);
+            profit = nums[i] - lst;
+            max = Math.max(max,profit);
+        }
+        return max;
+    }
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) throws IOException {
+
+        System.out.println("Enter the size of the array: \n");
+        int n = scanner.nextInt();
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        System.out.println("Enter the array of size " + n + "\n");
+        int[] arr = new int[n];
+
+        //String[] arrItems = scanner.nextLine().split("\n");
+        //scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        for (int i = 0; i < n; i++) {
+            arr[i] = scanner.nextInt();
+        }
+
+        int res = maxProfit(arr);
+        System.out.println(res);
+
+        scanner.close();
+    }
+}
